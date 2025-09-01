dd37c985b8614b2a775b510cddfcb66c8ad3e874
Niharika <niharika.mailme123@gmail.com>
2025-09-01 22:46:26 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,35 @@
+
+// let age = 12;
+// // let name = age>10 && "ihaeif";
+// let name = age>10 ? "Pedro": "pedmatro";
+
+
+// const person = {
+//     name:"Pedro",
+//     age : 41,
+//     isMarried: true,
+// };
+
+// const person2 ={...person, name:"Jack"}
+
+// // arrays addition
+
+// const arr1 = [1, 2, 4];
+// const arr2 = [...arr1, 3];
+
+
+// function of arrays:
+// .map()
+// .filter()
+// .reduce()
+
+let names = ["Pedro", "Jack", "Jessica", "Pedro"];
+// // react
+// names.map((name)=>{
+//     return <h1>{name}</h1>
+// })
+
+// to remove too many pedros:
+names.filter((name)=> {
+    return name !== "Pedro"
+})
